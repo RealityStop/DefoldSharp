8de83cbda4d9ebe64d367f8409eecffe7295eed6
RealityStop <realitydotstop@gmail.com>
2022-10-22 00:37:11 -0400

    added missing sprite.play_flipbook

--- src/defold/sprite.cs
+++ src/defold/sprite.cs
@@ -68,4 +68,103 @@ public static class sprite
 	public static extern void set_vflip(Url url_p1, bool flip_p2);
 	
 	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1})"
+	/// </summary>
+	public static extern void play_flipbook(string url_p1, Hash id_p2);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1}, {2})"
+	/// </summary>
+	public static extern void play_flipbook(string url_p1, Hash id_p2, Action<object,Hash,ILuaTable,Url> complete_function_p3);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1}, {2}, {3})"
+	/// </summary>
+	public static extern void play_flipbook(string url_p1, Hash id_p2, Action<object,Hash,ILuaTable,Url> complete_function_p3, ILuaTable play_properties_p4);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1})"
+	/// </summary>
+	public static extern void play_flipbook(Hash url_p1, Hash id_p2);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1}, {2})"
+	/// </summary>
+	public static extern void play_flipbook(Hash url_p1, Hash id_p2, Action<object,Hash,ILuaTable,Url> complete_function_p3);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1}, {2}, {3})"
+	/// </summary>
+	public static extern void play_flipbook(Hash url_p1, Hash id_p2, Action<object,Hash,ILuaTable,Url> complete_function_p3, ILuaTable play_properties_p4);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1})"
+	/// </summary>
+	public static extern void play_flipbook(Url url_p1, Hash id_p2);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1}, {2})"
+	/// </summary>
+	public static extern void play_flipbook(Url url_p1, Hash id_p2, Action<object,Hash,ILuaTable,Url> complete_function_p3);
+	
+	
+	/// <summary>
+	/// Play an animation on a sprite component from its tile set
+	/// An optional completion callback function can be provided that will be called when
+	/// the animation has completed playing. If no function is provided,
+	/// a <a href="#animation_done">animation_done</a> message is sent to the script that started the animation.
+	/// 
+	/// @CSharpLua.Template = "sprite.play_flipbook({0}, {1}, {2}, {3})"
+	/// </summary>
+	public static extern void play_flipbook(Url url_p1, Hash id_p2, Action<object,Hash,ILuaTable,Url> complete_function_p3, ILuaTable play_properties_p4);
+	
+	
 }
