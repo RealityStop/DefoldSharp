bdefa96e9fb32fcf1da59873318f09a9859484dc
RealityStop <realitydotstop@gmail.com>
2022-10-28 00:05:20 -0400

    Removing caching from built-in components.  No way to handle object desctruction

--- src/defold/support/ComponentReferences/BuiltInComponentBase.cs
+++ src/defold/support/ComponentReferences/BuiltInComponentBase.cs
@@ -4,20 +4,11 @@ namespace support
 {
 	public class BuiltInComponentBase : IBuiltInComponent
 	{
-		public bool IsCachingEnabled { get; private set; }
 		public Url Locator { get; private set; }
 		public void AssignLocator(ComponentLocator locator)
 		{
 			Locator = locator.FetchUrl();
 		}
-
-
-		public void EnableCaching()
-		{
-			IsCachingEnabled = false;
-		}
-		
-		
 
 		public static implicit operator Url(BuiltInComponentBase self)
 		{
