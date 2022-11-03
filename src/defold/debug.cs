21078475b646ac0b59a8cd2b1e4df8f2a8a76365
RealityStop <realitydotstop@gmail.com>
2022-11-03 09:14:13 -0400

    Fixed namespace and formatting for debug

--- src/defold/debug.cs
+++ src/defold/debug.cs
@@ -1,71 +1,70 @@
-﻿namespace DefaultNamespace
+﻿public static class Debug
 {
-	public static class Debug
+	/// <summary>
+	///     @CSharpLua.Template = "sys.get_engine_info().is_debug"
+	/// </summary>
+	public extern static bool IsEditorDebug();
+
+
+	static Debug()
 	{
-		/// <summary>
-		///     @CSharpLua.Template = "sys.get_engine_info().is_debug"
-		/// </summary>
-		public extern static bool IsEditorDebug();
-
-
-		static Debug()
+		if (IsEditorDebug())
 		{
-			if (IsEditorDebug())
-			{
-				_debuggingEnabled = true;
-				
-				_hook = _getHook(out _mask, out _count);
-			}
+			_debuggingEnabled = true;
+
+			_hook = _getHook(out _mask, out _count);
 		}
+	}
 
 
-		/// <summary>
-		///     @CSharpLua.Template = "debug.gethook()"
-		/// </summary>
-		private static extern object _getHook(out object mask, out object count);
-		
-		
-		/// <summary>
-		///     @CSharpLua.Template = "debug.sethook()"
-		/// </summary>
-		private static extern object _setHook();
-		
-		/// <summary>
-		///     @CSharpLua.Template = "debug.sethook({0},{1},{2})"
-		/// </summary>
-		private static extern object _setHook(object hook, object mask, object count);
-		
-
-		private static object _hook;
-		private static object _mask;
-		private static object _count;
-		private static bool _debuggingEnabled = true;
+	/// <summary>
+	///     @CSharpLua.Template = "debug.gethook()"
+	/// </summary>
+	private static extern object _getHook(out object mask, out object count);
 
 
-		public static void SetEditorDebug(bool shouldDebug = true)
+	/// <summary>
+	///     @CSharpLua.Template = "debug.sethook()"
+	/// </summary>
+	private static extern object _setHook();
+
+
+	/// <summary>
+	///     @CSharpLua.Template = "debug.sethook({0},{1},{2})"
+	/// </summary>
+	private static extern object _setHook(object hook, object mask, object count);
+
+
+	private static object _hook;
+	private static object _mask;
+	private static object _count;
+	private static bool _debuggingEnabled = true;
+
+
+	public static void SetEditorDebug(bool shouldDebug = true)
+	{
+		if (!IsEditorDebug())
+			return;
+
+		if (shouldDebug)
 		{
-			if (!IsEditorDebug())
-				return;
-			
-			if (shouldDebug)
-			{
-				_setHook(_hook, _mask, _count);
-				_debuggingEnabled = true;
-			}
-			else
-			{
-				_setHook();
-				_debuggingEnabled = false;
-			}
+			_setHook(_hook, _mask, _count);
+			_debuggingEnabled = true;
 		}
-		
-		public static bool ToggleEditorDebug()
+		else
 		{
-			if (!IsEditorDebug())
-				return false;
-
-			SetEditorDebug(!_debuggingEnabled);
-			return _debuggingEnabled;
+			_setHook();
+			_debuggingEnabled = false;
 		}
+	}
+
+
+	public static bool ToggleEditorDebug()
+	{
+		if (!IsEditorDebug())
+			return false;
+
+		SetEditorDebug(!_debuggingEnabled);
+		return _debuggingEnabled;
 	}
 }
