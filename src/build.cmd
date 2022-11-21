b86ee68b33ccedc5b466a36d0d6377fc4ed1b700
RealityStop <realitydotstop@gmail.com>
2022-11-20 20:24:44 -0500

    default build command should enable lua debug

--- src/build.cmd
+++ src/build.cmd
@@ -1,1 +1,1 @@
-..\\bin\\compiler\\CSharp.lua.Launcher.exe -s .\\ -d ..\\scripts -c -include ..\\bin\\CoreSystem.Lua -metadata -inline-property -p 
+..\\bin\\compiler\\CSharp.lua.Launcher.exe -s .\\ -d ..\\scripts -c -include ..\\bin\\CoreSystem.Lua -metadata -inline-property
